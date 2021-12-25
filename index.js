128372b54d1ac1a647684558a0023339d7947401
somu0 <94071371+somu0@users.noreply.github.com>
2021-12-25 17:31:27 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+num1=10
+num2=20
+num3=15
+ if (num1>=num2&&num1>=num3){
+  console.log("a is a greater  number");
+}else if(num2>=num3){
+  console.log("b is a greater number");
+}else{
+  console.log("c is a greater number");
+}
